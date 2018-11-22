e601c3630afca142bb248b1bde062ead9ccda35e
LuckyShawn <LuckyShawn@foxmail.com>
2018-11-22 09:44:16 +0800

    睡眠排序算法（娱乐）

--- config-server/src/main/java/com/shawn/configserver/ArraySort.java
+++ config-server/src/main/java/com/shawn/configserver/ArraySort.java
@@ -5,6 +5,29 @@ package com.shawn.configserver;
  * @Author: yang.xiao
  * @Date: 2018/11/22 0022
  */
-public class ArraySort {
+public class ArraySort implements Runnable{
 
+    private int number = 0;
+
+    public ArraySort(int number){
+        this.number = number;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{100,500,200,15,36,80,2500};
+        for(int number : numbers){
+            new Thread(new ArraySort(number)).start();
+        }
+    }
+
+    @Override
+    public void run(){
+        try {
+            Thread.sleep(this.number);
+            System.out.println(this.number);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
